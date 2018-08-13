e38491c22064444a43fb15550e90e7d49fec4190
Sarvesh Upadhyay <sarvesh.a.upadhyay@gmail.com>
2018-08-13 14:34:34 -0400

    Update: only download linux-simulator

--- download_resources.sh
+++ download_resources.sh
@@ -3,13 +3,15 @@ wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/58ae46bb_linux-s
 unzip linux-sim.zip
 rm linux-sim.zip
 
+# old linux simulator
+# requires slack login
 # wget https://files.slack.com/files-pri/T2HQV035L-F3A47CT7Z/download/simulator-linux.zip
 
-cd ./data
+# cd ./data
 
 # sample training data (317.70 MB)
-wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip
-unzip data.unzip
-mv data default_data
-rm data.unzip
-rm -r ./__MACOSX
+# wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip
+# unzip data.unzip
+# mv data default_data
+# rm data.unzip
+# rm -r ./__MACOSX
